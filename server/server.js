f5398687da017995d7e9366f66966d9ab6bf14a5
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2023-12-24 13:20:28 +0200

    client run

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoUrl)
         console.log(error);
     })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listen on port ${PORT}`);
 })
