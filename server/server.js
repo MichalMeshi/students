e8428e117a763ceac9bd24e6ed56bf609cbb93f2
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2023-12-24 13:40:27 +0200

    front and back run good

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoUrl)
         console.log(error);
     })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log(`Listen on port ${PORT}`);
 })
