7d1efa3b6593b1bb86071fd2b95dbd8ba3f60408
mgranot1 <michalgranot100@gmail.com>
2023-12-25 10:10:07 +0200

    add CourseDatialPage

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -4,6 +4,8 @@ import Home from '../pages';
 import NoPage from './NoPage'
 import Register from './Register';
 import Login from './Login';
+import CourseDatialPage from '../pages/CourseDatialPage';
+
 const AppRoutes = () => {
 
     return (
@@ -11,6 +13,8 @@ const AppRoutes = () => {
             <Route path='/' element={<Home />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/courses/course_details' element={<CourseDatialPage/>}></Route> 
+
             <Route path='*' element={<NoPage />} />
         </Routes>
     )
--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import CourseDatialPage from '../pages/CourseDatialPage';
 
 export default function CourseCard(props) {
     const {course}=props;
@@ -10,7 +12,7 @@ export default function CourseCard(props) {
     <h5 className="card-title">{course.name}</h5>
     <h6 className="card-subtitle mb-2 text-body-secondary">{course.field}</h6>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="card-link">read more</a>
+    <Link className="card-link" to='/courses/course_details'>read more</Link>
   </div>
 </div>
   )
