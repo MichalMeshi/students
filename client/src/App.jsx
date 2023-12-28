93dbc8d6e05ab37ada935cb9bb7da319940d7283
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2023-12-28 11:43:25 +0200

    fix meginng

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppRoutes from './components/AppRoutes'
 import CourseDatialPage from './pages/CourseDatialPage';
+import { ProfileContextProvider } from './context/ProfileContext';
+import { ForumContextProvider } from './context/ForumContext';
 
 function App() {
 
   return (
-    <ProfileContextProvider>
-      <CourseContextProvider>
-        <BrowserRouter>
-          <Navbar />
-          <AppRoutes />
-        </BrowserRouter>
-      </CourseContextProvider>
-    </ProfileContextProvider>
+    <ForumContextProvider>
+      <ProfileContextProvider>
+        <CourseContextProvider>
+          <BrowserRouter>
+            <Navbar />
+            <AppRoutes />
+          </BrowserRouter>
+        </CourseContextProvider>
+      </ProfileContextProvider>
+    </ForumContextProvider>
   )
 }
 
