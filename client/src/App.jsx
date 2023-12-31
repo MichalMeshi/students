d6e47c82e322c13fa2d7c116da8d6f7b861593da
mgranot1 <michalgranot100@gmail.com>
2023-12-31 11:05:42 +0200

    merge success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,27 +6,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppRoutes from './components/AppRoutes'
 import CourseDatialPage from './pages/CourseDatialPage';
 import { ProfileContextProvider } from './context/ProfileContext';
-<<<<<<< HEAD
-import SummaryContext, { SummaryContextProvider } from './context/SummaryContext';
-=======
 import { ForumContextProvider } from './context/ForumContext';
->>>>>>> a0a6a200a06b18b7ee80d7d997ef0bb2e643e7aa
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <ProfileContextProvider>
-      <CourseContextProvider>
-        <SummaryContextProvider>
-        <BrowserRouter>
-          <Navbar />
-          <AppRoutes />
-        </BrowserRouter>
-        </SummaryContextProvider>
-      </CourseContextProvider>
-    </ProfileContextProvider>
-=======
     <ForumContextProvider>
       <ProfileContextProvider>
         <CourseContextProvider>
@@ -37,7 +21,6 @@ function App() {
         </CourseContextProvider>
       </ProfileContextProvider>
     </ForumContextProvider>
->>>>>>> a0a6a200a06b18b7ee80d7d997ef0bb2e643e7aa
   )
 }
 
