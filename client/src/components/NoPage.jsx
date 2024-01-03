f19ee218b606820cb02e113298ce0d2cc8083b80
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2024-01-03 19:35:58 +0200

    Update NoPage.jsx

--- client/src/components/NoPage.jsx
+++ client/src/components/NoPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import '../stylesheets/bluebutton.css'
 
 const NoPage = () => {
   return (
-    <div>NoPage</div>
+    <div className =''style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1 style={{ color: 'blue' }}>404 Not Found</h1>
+      <p>This page doesn't exist.</p>
+      <div>
+      <img src='https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif' width={200}/>
+
+      </div>
+      <Button id = 'loginbtn'>Home</Button>
+    </div>
   )
 }
 
