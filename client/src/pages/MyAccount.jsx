37e400244fbb037aeffb29dfd8802e42ec63188f
nosulima <noasuliman131@gmail.com>
2024-01-03 13:04:09 +0200

    profile commit

--- client/src/pages/MyAccount.jsx
+++ client/src/pages/MyAccount.jsx
@@ -3,13 +3,21 @@ import Profile from '../components/Profile'
 import CourseCardList from '../components/CourseCardList'
 import MyCourses from '../components/MyCourses'
 import Navbar from '../components/Navbar'
+import '../stylesheets/myAccount.css'
 const MyAccount = () => {
+
+
   return (
-    <div className='container'>
+    <>
       <Navbar />
-      <Profile />
-      <MyCourses />
-    </div>
+      <div className='container-fluid'>
+        <div id='profile-strip' className='container'>
+          <Profile />
+        </div>
+        </div>
+        <MyCourses />
+      
+    </>
   )
 }
 
