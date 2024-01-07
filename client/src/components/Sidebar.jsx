025a90b7ee57f224c6551adfe7aadf8ecad54e81
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2024-01-07 09:43:38 +0200

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../stylesheets/sidebar.css'
+import { GiHamburgerMenu } from 'react-icons/gi';
+const Sidebar = ({ courseId, isOpen, setIsOpen }) => {
 
-export default function Sidebar({ courseId }) {
-
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav>
-      <Link to={`/course_details/${courseId}/summeries`}>Summeries </Link>
-      <Link to={`/course_details/${courseId}/forum`}>Forum </Link>
-      <Link to={`/course_details/${courseId}/sharedLearning`}>Shared Learning </Link>
-    </nav>
-  )
-}
+    <>
+      <Button onClick={toggleSidebar} variant='light'>
+        <GiHamburgerMenu />
+      </Button>
+      <Offcanvas show={isOpen} onHide={toggleSidebar} placement="start" backdrop={false} scroll={true}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Navbar bg="light" data-bs-theme="light" className='d-flex flex-column'>
+            <Container className="flex-column">
+              <Navbar.Brand href={`/course_details/${courseId}`}>Course</Navbar.Brand>
+              <Nav className="me-auto flex-column">
+                <Nav.Link href={`/course_details/${courseId}/summeries`}>Summeries</Nav.Link>
+                <Nav.Link href={`/course_details/${courseId}/forum`}>Forum</Nav.Link>
+                <Nav.Link href={`/course_details/${courseId}/sharedLearning`}>Shared Learning</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+
+  );
+};
+
+export default Sidebar;
