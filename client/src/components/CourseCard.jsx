238a6a6d280de457d174259db4708a49f758f312
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2024-01-08 10:26:37 +0200

    course card

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -29,19 +29,6 @@ export default function CourseCard(props) {
 
 
   return (
-    // <div className="card">
-    //   <div className="card-body">
-    //     <h5 className="card-title">{course.name}</h5>
-    //     <h6 className="card-subtitle mb-2 text-body-secondary">{course.field}</h6>
-    //     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //     <Link className="card-link" to={`/course_details/`+course._id}>read more</Link>
-    //     {favorite ? (
-    //       <IoMdHeart onClick={onUnFavorite} style={{ cursor: "pointer" }} />
-    //     ) : (
-    //       <IoIosHeartEmpty onClick={onFavorite} style={{ cursor: "pointer" }} />
-    //     )}
-    //   </div>
-    // </div>
     <Card id="course-card" style={{ width: '20rem',height:'100%' }}  data-aos="fade-up" >
       <Card.Img variant="top" src={course.img} />
       <Card.Body>
