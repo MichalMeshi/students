3571e6993130f590696deff699cbeffe7e62ac64
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2024-01-17 16:26:51 +0200

    email

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,24 +1,29 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (options, senderEmail,htmlContent) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT || 2525,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+const sendEmail = async (options, senderEmail, htmlContent) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT || 2525,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: `${senderEmail}`, // Use the sender's email as the "from" address
-    to: options.email,
-    subject: options.subject,
-    text: options.text,
-    html: options.htmlContent   // This will include the HTML content in the email
-  };
+    const mailOptions = {
+      from: `${senderEmail}`, // Use the sender's email as the "from" address
+      to: options.email,
+      subject: options.subject,
+      text: options.text,
+      html: options.htmlContent   // This will include the HTML content in the email
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = sendEmail;
